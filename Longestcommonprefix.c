a08192d6bd86bbab8cfdeaba7420717627b380b9
thrilokya1202 <95009492+thrilokya1202@users.noreply.github.com>
2023-02-03 18:22:10 +0530

    Update Longestcommonprefix.c

--- Longestcommonprefix.c
+++ Longestcommonprefix.c
@@ -47,9 +47,10 @@ char * longestCommonPrefix(char ** strs, int strsSize){
 void main()
 {
 int strsSize;
-printf("Enter number of Strings");
+printf("Enter number of Words");
 scanf("%d",&strsSize);
 char ** strs=malloc(strsSize*sizeof(char*));
+printf("Enter Words");
 for(int i=0;i<strsSize;i++)
 {
 strs[i]=malloc(50*sizeof(char));
@@ -58,3 +59,11 @@ scanf("%s",strs[i]);
 char * r=longestCommonPrefix(strs,strsSize);
 printf("Longest Common Prefix :%s",r);
 }
+
+Output:
+Enter number of Words4
+Enter Wordsflower
+flight
+fly
+flow
+Longest Common Prefix :fl
